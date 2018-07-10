80102031d1cd0f952e48a4b084e243d16a962690
Mate Antičević <mate@anticevic.net>
2018-07-10 20:58:12 +0200

    Initial commit

---
+++ ProjectIvy.Messages.Models/Trackings/Tracking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ProjectIvy.Messages.Models.Trackings
+{
+    public class Tracking
+    {
+        [JsonProperty("ac")]
+        public decimal? Accuracy { get; set; }
+
+        [JsonProperty("al")]
+        public decimal? Altitude { get; set; }
+
+        [JsonProperty("lt")]
+        public decimal Latitude { get; set; }
+
+        [JsonProperty("ln")]
+        public decimal Longitude { get; set; }
+
+        [JsonProperty("t")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty("s")]
+        public decimal? Speed { get; set; }
+
+        [JsonProperty("u")]
+        public int UserId { get; set; }
+    }
+}
---
+++ ProjectIvy.Messages/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using Newtonsoft.Json;
+using ProjectIvy.Messages.Models.Trackings;
+
+namespace ProjectIvy.Messages.QueueListener
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "do.anticevic.net" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "trackings", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var tracking = JsonConvert.DeserializeObject<Tracking>(message);
+                        Console.WriteLine(" [x] Received {0}", tracking.Latitude);
+                    };
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
